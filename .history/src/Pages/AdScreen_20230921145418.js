9c6255556d9c5951104dfb84771e18deb947623e
React1 <React1@flyweis.technology>
2023-09-21 15:26:06 +0530

    changes

---
+++ .history/src/Pages/AdScreen_20230921145418.js
@@ -0,0 +1,97 @@
+/** @format */
+
+import axios from "axios";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+
+const AdScreen = () => {
+  const { id } = useParams()
+  const [ data , setData ] = useState({})
+
+  const fetchHandler = async () => {
+    try{
+      const { res } = await axios.get(~)
+    }catch{}
+  }
+
+  return (
+    <>
+      <div className="Banner AboutUs addd">
+        <div className="content" style={{ width: "80%", margin: "auto" }}>
+          <h2
+            className="permanent-job-heading"
+            style={{ lineHeight: "normal" }}
+          >
+            Launching Our Brand New App!
+          </h2>
+          <h2 className="permanent-job-heading" style={{ fontSize: "42px" }}>
+            VERY SOON !!{" "}
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget
+            mollis velit. Praesent pharetra, ipsum eget vestibulum eleifend, est
+            sapien mollis erat, faucibus rhoncus purus tellus eget odio. Nam
+            dapibus magna a neque luctus auctor quis quis diam. Donec viverra
+            laoreet massa quis consectetur.
+          </p>
+        </div>
+      </div>
+
+      <h1 className="bold-heading">OUR NEW APP</h1>
+
+      <div className="Staff_Desc">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tempus
+          eleifend ullamcorper. Sed maximus nunc vitae metus pharetra, quis
+          pharetra felis iaculis. Aenean in nisl eget lorem congue efficitur id
+          ut orci. Mauris volutpat tortor non lectus rhoncus vestibulum bibendum
+          quis leo. Nulla lobortis feugiat nibh. Mauris pulvinar quam nec lectus
+          ornare, id auctor nulla venenatis. Duis sit amet rhoncus lacus. Proin
+          nisi dolor, posuere mattis viverra vel, dignissim et augue.
+          Suspendisse convallis nec neque et tincidunt. Vestibulum ante ipsum
+          primis in faucibus orci luctus et ultrices posuere cubilia curae;{" "}
+        </p>
+      </div>
+
+      <div className="About_Us-two_Sec">
+        <div className="left">
+          <img src="./Image/Group 1000000932.png" alt="" />
+        </div>
+        <div className="right">
+          <p style={{ textAlign: "justify" }}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tempus
+            eleifend ullamcorper. Sed maximus nunc vitae metus pharetra, quis
+            pharetra felis iaculis. Aenean in nisl eget lorem congue efficitur
+            id ut orci. Mauris volutpat tortor non lectus rhoncus vestibulum
+            bibendum quis leo. Nulla lobortis feugiat nibh. Mauris pulvinar quam
+            nec lectus ornare, id auctor nulla venenatis. Duis sit amet rhoncus.{" "}
+            <br />
+            <br />
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tempus
+            eleifend ullamcorper. Sed maximus nunc vitae metus pharetra, quis
+            pharetra felis iaculis. Aenean in nisl eget lorem congue efficitur
+            id ut orci. Mauris volutpat tortor non lectus rhoncus vestibulum
+            bibendum quis leo. Nulla lobortis feugiat nibh. Mauris pulvinar quam
+            nec lectus ornare, id auctor nulla venenatis. Duis sit amet rhoncus.
+          </p>
+        </div>
+      </div>
+
+      <div style={{ width: "90%", margin: "40px auto" }}>
+        <iframe
+          width="100%"
+          height="500"
+          src="https://www.youtube.com/embed/JxZ9iqWVlSE?si=InTXwsXs3JbTwAMf&amp;start=3"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+      </div>
+
+      <div className="mb-5"></div>
+    </>
+  );
+};
+
+export default AdScreen;
